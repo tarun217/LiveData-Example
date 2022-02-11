053284c7ce692ade4cde9fe7b3b7f977424c032d
Tarun217 <tarun.patel@sayalife.in>
2022-02-11 14:41:47 +0530

    Revert "On off button using Live data"
    This reverts commit c674e65c

--- ViewModelEx/app/src/main/java/com/example/viewmodelex/ViewModels.java
+++
@@ -1,9 +0,0 @@
-package com.example.viewmodelex;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class ViewModels extends ViewModel {
-    MutableLiveData<Boolean> currentState = new MutableLiveData<>(false);
-
-}
